15c1a3973f95442233eb96c7cd9ef0a19063e8ef
ankiiitraj <ankitatiiitr@gmail.com>
2020-10-16 01:58:49 +0530

    Nav option deletion

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -17,7 +17,6 @@ const Nav = (props) => {
 						<div className="dropdown">
 							<button disabled={token ? false : true} onClick={()=>{handleClick(!disabled)}} className="nav-button">Hello {props.user.name}! <span><Down height="15px" width="15px" /></span></button>
 							<div onClick={()=>{handleClick(!disabled)}} style={{display: (disabled ? "none" : "block")}} className="dropdown-items">
-								<div className="dropdown-item"><Link to={`/${props.user.name}`}>My Profile</Link></div>
 								<div className="dropdown-item"><Link onClick={() => {window.localStorage.clear(); props.signout()}} to="/">Sign out</Link></div>
 							</div>
 						</div>
