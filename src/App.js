b8a42d827276f9ec7f7ba19048184fc78c350415
Ankit Raj <ankitatiiitr@gmail.com>
2022-05-03 12:33:43 +0530

    remove top notify bar

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import About from "./components/about/About";
 import Footer from "./components/footer/Footer";
 import {   NotificationManager, NotificationContainer } from "react-notifications";
 import Lists from "./components/lists/lists";
-import NotifyTopBar from "./components/Notify/bar";
+// import NotifyTopBar from "./components/Notify/bar";
 
 function App() {
   const [user, setUserDetail] = useState({ name: window.localStorage.getItem('name') ||"user", email: window.localStorage.getItem('email') || "N/A" });
@@ -81,7 +81,7 @@ function App() {
   return (
     <div className="App">
       <div className="main">
-        <NotifyTopBar />
+//         <NotifyTopBar />
         {renderNav()}
         <Switch>
           <ProtectedRoute
